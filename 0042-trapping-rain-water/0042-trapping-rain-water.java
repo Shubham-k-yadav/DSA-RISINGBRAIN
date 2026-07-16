ad5fbb290ed95bceba70aeee8ee08baca3d84de5
Shubham Kumar Yadav <shubhamyk6369@gmail.com>
2026-07-16 08:54:13 +0530

    Time: 0 ms (100%), Space: 47.8 MB (57.29%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,27 +1,38 @@
 class Solution {
-    public int trap(int[] height) {
-        int larr[] = new int[height.length];
-        int rarr[] = new int[height.length];
-
-        larr[0] = height[0];
-        for(int i=1;i<height.length ;i++)
+    public int trap(int[] nums) {
+        int l = 0;
+        int r = nums.length-1;
+        int lmax = 0;
+        int rmax = 0;
+        int w = 0;
+        while(l<r)
         {
-            larr[i] = Math.max(larr[i-1],height[i]);
+            if(nums[l]<nums[r])
+            {
+                if(nums[l]>=lmax)
+                {
+                    lmax = nums[l];
+                }
+                else
+                {
+                    w += lmax - nums[l]; 
+                }
+                l++;
+            }
+
+            else
+            {
+                if(nums[r]>=rmax)
+                {
+                    rmax = nums[r];
+                }
+                else
+                {
+                    w += rmax - nums[r]; 
+                }
+                r--;
+            }
         }
-
-        rarr[height.length-1] = height[height.length-1];
-
-        for(int i=height.length-2;i>= 0 ;i--)
-        {
-            rarr[i] = Math.max(rarr[i+1],height[i]);
-        }
-
-        int water = 0;
-
-        for(int i=0;i<height.length;i++)
-        {
-            water += Math.min(larr[i],rarr[i])-height[i];
-        }
-        return water;
+        return w;
     }
 }
